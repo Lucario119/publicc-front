8b13e69b39d8a20b1bf2fafaa9afc9020179956c
Lucas de Oliveira <lucasoliveira13670@gmail.com>
2021-11-04 22:43:11 -0400

    novas funcionalidades

--- src/pages/cards.jsx
+++ src/pages/cards.jsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/pages/cards.module.css';
-import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
+import {
+  Draggable,
+  Droppable,
+  DragDropContext,
+  DropResult,
+} from 'react-beautiful-dnd';
 import Asset1 from '../assets/Sem títulodfdf.png';
 import Image from 'next/image';
 
@@ -21,7 +26,10 @@ function Cards() {
   ];
 
   const [isDragging, setIsDragging] = useState(false);
+  const [isDragEnded, setIsDragEnded] = useState(false);
   const [items, setItems] = useState([]);
+  const [items2, setItems2] = useState([]);
+
   useEffect(() => {
     setItems(cards);
   }, []);
@@ -30,13 +38,17 @@ function Cards() {
     !isDragging ? setIsDragging(true) : setIsDragging(false);
   }
   function onDragEnd(id) {
-    dragState();
     items.splice(id, 1);
+
     if (items.length === 0) {
       const newArray = [...cards];
 
       setItems(newArray);
     }
+    if (items2.length < 7) {
+      items2.push(1);
+    }
+    setIsDragEnded(true);
   }
 
   return (
@@ -44,58 +56,71 @@ function Cards() {
       <div className={styles.cardsContainer}>
         <Droppable droppableId="droppable">
           {(provided) => (
-            <>
-              <div className={styles.cardContainer}>
-                <>
-                  {items.slice(0, 1).map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.id.toString()}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          key={item.id}
-                          id={item.id}
-                          className={item.className}
-                          onDrag={dragState}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          ref={provided.innerRef}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
-                </>
-              </div>
-              <div className={styles.dropzoneContainer}>
-                <div
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className={styles.dropzone}
-                >
-                  <div>
-                    {isDragging && (
-                      <h1 className={styles.blur}>
-                        PRIMEIRA AÇÃO DE CHAMAR .FICA EM MODO BLUR ASSIM QUE
-                        SOLTAR
-                      </h1>
+            <div
+              className={styles.cardContainer}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <>
+                {items.slice(0, 1).map((item, index) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={item.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        key={item.id}
+                        id={item.id}
+                        onDrag={dragState}
+                        className={item.className}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      />
                     )}
-                  </div>
-                  <Image src={Asset1} alt="asset" width={1000} height={100} />
-
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                  {/* {provided.placeholder} */}
-                </div>
-              </div>
-            </>
+                  </Draggable>
+                ))}
+              </>
+            </div>
           )}
         </Droppable>
+
+        <div />
+        <div className={styles.dropzoneContainer}>
+          <Droppable droppableId="trashbin">
+            {(provided) => (
+              <div
+                className={styles.dropzone}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <div>
+                  {isDragEnded && (
+                    <h1 className={styles.blur}>
+                      PRIMEIRA AÇÃO DE CHAMAR .FICA EM MODO BLUR ASSIM QUE
+                      SOLTAR
+                    </h1>
+                  )}
+                </div>
+                <Image src={Asset1} alt="asset" width={1000} height={100} />
+                {items2.map((e, i) => (
+                  <div key={i}>
+                    {[...Array(1)].map((e, i) => (
+                      <>
+                        <span key={i} className={styles.blur}>
+                          PRIMEIRA AÇÃO DE CHAMAR .FICA EM MODO BLUR ASSIM QUE
+                          SOLTAR
+                        </span>
+                      </>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </div>
+        {/* {provided.placeholder} */}
       </div>
     </DragDropContext>
   );
